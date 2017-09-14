cf4acacc0e2ace8e80a6161e25856b4b94d45cbf
pancake <88520tony@gmail.com>
2017-09-14 14:42:22 +0800

    third1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-System.out.print("學號:106022016\n系級:行動商務與多媒體應用學系\n姓名:張秉淞\n時間:2017/09/14\n地點:\"i319\"");
+System.out.print("學號:106022016\n系級:行動商務與多媒體應用學系\n姓名:張秉淞\n
 
 	// write your code here
     }
