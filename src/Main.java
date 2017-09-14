9e4ab12ac3df97c547606e58c73151fd0f1daeae
pancake <88520tony@gmail.com>
2017-09-14 15:14:44 +0800

    forth

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-System.out.print("學號:106022016\n系級:行動商務與多媒體應用學系\n姓名:張秉淞\n
-
+   Scanner input=new Scanner(System.in);
+   int number1, number2, sum;
+   System.out.print("請輸入第一個數:");
+   number1=input.nextInt();
+   System.out.print("請輸入第二個數:");
+   number2=input.nextInt();
+   sum=number1+number2;
+   System.out.printf("%d+%d=%d", number1,number2,sum);
+   input.close();
+System.out.print("");
 	// write your code here
     }
 }
+
+
+
+
