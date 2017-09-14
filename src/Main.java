2e6355eb4ebc532061211987ffeb6a49eed079da
pancake <88520tony@gmail.com>
2017-09-14 14:40:59 +0800

    third

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-System.out.print("My First Java Code!!")
+System.out.print("學號:106022016\n系級:行動商務與多媒體應用學系\n姓名:張秉淞\n時間:2017/09/14\n地點:\"i319\"");
 
 	// write your code here
     }
